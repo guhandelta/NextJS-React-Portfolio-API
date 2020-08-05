9178cb3f07bd5b7830eff4cbc66677b9af1d3d17
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-08-05 10:31:43 -0400

    Created a MongoDB database and connected the application to teh db using mongoose

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const server = express();
+const mongoose = require('mongoose')
 
+const config = require('./config/dev')
+
+mongoose.connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err) => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log('Çonnected to database!');
+    }
+})
 
 // This is one way of joining routes. This endpoint is for the route /api/v1/portfolios, after reaching this route, the navigation jumps-
 //- over to portfolios.js, which takes care of the rest routing, depending upon the route specified after /api/v1, in the req
