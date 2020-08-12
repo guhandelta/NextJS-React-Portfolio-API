2dec48916415d54f2a7f7e43258cdcba88c65e80
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-08-11 23:18:41 -0400

    Created Middleware to check if the user trying to access the POST endpoint for creating a portfolio, is an Authorized user

---
+++ controllers/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('express-jwt');
+const jwksRSA = require('jwks-rsa');
+
+
+// Auth middleware | Checks for access token in authorization headers of req & verifies the token with Auth0 JSON Web Key Set(JWKS)
+exports.checkJWT = jwt({
+    secret: jwksRSA.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 10,
+        jwksUri: 'https://guhaprasaanth.auth0.com/.well-known/jwks.json' // Some keys provided by Auth0
+    }),
+    audience: 'https://guhaprasaanth.auth0.com/api/v2/',
+    issuer: 'https://guhaprasaanth.auth0.com/',
+    algorithms: ['RS256']
+})
--- routes/portfolios.js
+++ routes/portfolios.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 const { createPortfolio, getPortfolios, getPortfolioById } = require('../controllers/portfolios');
 
+// Middleware to check if the user is authenticated/admin
+const { checkJWT } = require('../controllers/auth');
+
 // Path is empty, as /api/v1/portfolios is provided/appended at getPortfolios
 router.get('', getPortfolios);
 router.get('/:id', getPortfolioById);
 
-router.post('', createPortfolio)
+// Adding the auth middleware, allows only the authenticated users to access this endpoint
+router.post('', checkJWT, createPortfolio)
 
 module.exports = router;
