043c5839d8000a44b2a16549019fca3ddbc44504
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-09-01 19:17:02 -0400

    Added authorization to the endpoints to create, update and delete the portfolio, to allow only the admin user to perform those tasks

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('express-jwt');
 const jwksRSA = require('jwks-rsa');
-
+const config = require('../config/dev')
 
 // Auth middleware | Checks for access token in authorization headers of req & verifies the token with Auth0 JSON Web Key Set(JWKS)
 exports.checkJWT = jwt({
@@ -13,4 +13,16 @@ exports.checkJWT = jwt({
     audience: 'https://guhaprasaanth.auth0.com/api/v2/',
     issuer: 'https://guhaprasaanth.auth0.com/',
     algorithms: ['RS256']
-}); 
+});
+
+
+// Auth middleware to verify the user role
+exports.checkRole = role => (req, res, next) => {
+    const user = req.user; // user will be available as checkJWT is used
+
+    if (user && user[config.AUTH0_NAMESPACE + '/roles'].includes(role)) {
+        next();
+    } else {
+        return res.status(401).send('You are not authorized to access this data');
+    }
+}
--- routes/portfolios.js
+++ routes/portfolios.js
@@ -10,17 +10,17 @@ const {
 } = require('../controllers/portfolios');
 
 // Middleware to check if the user is authenticated/admin
-const { checkJWT } = require('../controllers/auth');
+const { checkJWT, checkRole } = require('../controllers/auth');
 
 // Path is empty, as /api/v1/portfolios is provided/appended at getPortfolios
 router.get('', getPortfolios);
 router.get('/:id', getPortfolioById);
 
 // Adding the auth middleware, allows only the authenticated users to access this endpoint
-router.post('', checkJWT, createPortfolio);
+router.post('', checkJWT, checkRole('admin'), createPortfolio);
 
-router.patch('/:id', checkJWT, updatePortfolio);
+router.patch('/:id', checkJWT, checkRole('admin'), updatePortfolio);
 
-router.delete('/:id', checkJWT, deletePortfolio);
+router.delete('/:id', checkJWT, checkRole('admin'), deletePortfolio);
 
 module.exports = router;
