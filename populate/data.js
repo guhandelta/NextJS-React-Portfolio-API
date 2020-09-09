2dd56555eb66851ebc0f3c75c849fb2413d7c073
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-09-09 18:47:43 -0400

    Updated the experience JSON with updated complete data

--- populate/data.js
+++ populate/data.js
@@ -37,13 +37,112 @@ const data = {
             title: 'Web Developer',
             company: 'Marshall Recreation Center',
             companyWebsite: 'https://www.marshall.edu/campusrec/',
-            location: 'USA, West Virginia',
+            location: 'Huntington, West Virginia',
             jobTitle: 'Web Developer',
             description: 'Design and maintain databases using Python and developed Python-based API (RESTful Web Service) using Flask, SQL Alchemy and PostgreSQL <br />• Developed responsive component based UI for the application using React JS, Bootstrap. <br />• Developed the jQuery callback functions to implement asynchronous communication using AJAX.',
             userId: 'google-oauth2|101848771434318992569',
             startDate: '05/01/2015',
             endDate: '02/01/2016'
-        }
+        },
+        {
+            title: 'IOT Developer',
+            company: 'WV Aquavit - Marshall Universiy',
+            companyWebsite: 'https://wvaquavit.marshall.edu/',
+            location: 'Huntington, West Virginia',
+            jobTitle: 'IOT Developer',
+            description: '•	Developed a wireless sensor network using Raspberry Pi 3, Arduino <br /> •	Attached and configures a Vernier Water pH sensor to actively read and report water pH value, of Ohio River <br /> •	IBM Bluemix was the cloud platform use here to receive the water pH values and report the project admin about any changes in the pH level <br /> ',
+            userId: 'google-oauth2|101848771434318992569',
+            startDate: '03/01/2016',
+            endDate: '05/01/2016'
+        },
+        {
+            title: 'Web Developer',
+            company: 'Stadium Sports',
+            companyWebsite: '',
+            location: 'Huntington, West Virginia',
+            jobTitle: 'Web Developer',
+            description: '•	Developed RESTful services with NodeJS Loopback Framework. <br /> •	Created Node.JS middleware application server to encapsulate modern JS widget framework. <br /> •	Developed the Front end / UI components using: HTML5, CSS3, CSS Preprocessors(such as: Sass and LESS), JavaScript. <br /> •	Made the website’s UI responsive using Bootstrap4, along with AJAX. <br /> •	Designed dynamic client - side JavaScript codes to build web forms and simulate process for a web application, page navigation using React JS <br /> ',
+            userId: 'google-oauth2|101848771434318992569',
+            startDate: '07/01/2015',
+            endDate: '12/01/2016'
+        },
+        {
+            title: 'Web Developer',
+            company: 'Mildred Mitchell-Bateman Hospital',
+            companyWebsite: 'http://batemanhospital.org/',
+            location: 'Huntington, West Virginia',
+            jobTitle: 'Web Developer',
+            description: '•	Designed a secure responsive web interface using Bootstrap4 for viewing the website easily on any device. <br/> •	Architecting a fast, robust back - end using Python Django to record the posts and intimate the personnel about any new maintenance requests and the pre - registered maintenance dates <br/> •	Django ORM was used here to manage all the equipment and user information, over SQL lite 3 DB <br/> •	Utilized the Django templates to design robust user interface for the web application <br/> ',
+            userId: 'google-oauth2|101848771434318992569',
+            startDate: '01/01/2017',
+            endDate: '05/01/2017'
+        },
+        {
+            title: 'Fullstack Java Developer',
+            company: 'N2 Services',
+            companyWebsite: 'https://n2sglobal.com/',
+            location: 'Jacksonville, FL',
+            jobTitle: 'Fullstack Java Developer',
+            description: '•	Implemented Angular app with CRUD features utilizing all concepts of Angular 4 i.e. Data binding, Components, Directives, and user-defined directives, Routing, Observables, Forms, Pipes, Services with Dependency Injection <br /> •	Worked with Angular 2 forms like Template Driven forms and Reactive forms(Dynamic Forms) to perform form validations both on server and client side <br /> •	Developed the business logic using Java Spring MVC and deployed components on Web Sphere Application server Bootstrap used along with Angular 2 in creating the Application. <br /> •	Used NodeJS for doing non - blocking design operation for server side code. <br /> •	Used Angular 2 framework in developing various UI components. <br /> ',
+            userId: 'google-oauth2|101848771434318992569',
+            startDate: '07/01/2017',
+            endDate: '11/01/2017'
+        },
+        {
+            title: 'Cloud Developer',
+            company: 'Veridic SOlutions',
+            companyWebsite: 'http://veridicsolutions.com//',
+            location: 'Pleasanton, CA',
+            jobTitle: 'Cloud Developer',
+            description: '•	Designed and managed public/private cloud infrastructures using Amazon Web Services (AWS). <br /> •	Deployed Web Applications into Amazon AWS by configuring the Elastic Beanstalk application and environment <br /> •	Worked On - Premise migration into AWS, over a dedicated connection using AWS Direct Connect <br /> •	Analyze AWS based product for defects, and enhance automated testing to prevent regression. <br /> •	Developed cookbooks using Chef for Deploying services on Hybrid environments, both Windows and Linux <br /> •	Developing a deployment management system for Docker Containers in AWS Elastic Container Service. <br /> •	Converted numerous existing Java projects to a single deployment method using ECS Docker Containers. <br /> •	Used Cloud Watch for monitoring AWS cloud resources and the applications that deployed on AWS by creating new alarm, enable notification service. <br /> ',
+            userId: 'google-oauth2|101848771434318992569',
+            startDate: '11/01/2017',
+            endDate: '03/01/2018'
+        },
+        {
+            title: 'Fullstack Developer',
+            company: 'Blue Shield of California',
+            companyWebsite: 'https://www.blueshieldca.com/',
+            location: 'San Francisco, CA',
+            jobTitle: 'Fullstack Developer',
+            description: '•	Actively involved in Requirement Gathering meetings with the Clients and Participated in project planning sessions with business analysts and team members to analyze business IT Requirements and translated business requirements into working model. br /> •	Implemented Single Page Application(SPA) using HTML5, CSS3, Bootstrap and Angular 4. br /> •	Participated in code review meetings and provided improvements. br /> •	Use SVN for software configuration management and version control and deploy the application in JBOSS server. br /> •	Developed custom JSON parsers to automatically discover the projects or schemas defined in the data source. br /> •	Developed internal web dashboard applications using Node JS. br /> •	Analyzed and fixed bugs reported in QTP and effectively delivered bug fixes reported with  quick turnaround time br /> ',
+            userId: 'google-oauth2|101848771434318992569',
+            startDate: '05/01/2018',
+            endDate: '11/01/2018'
+        },
+        {
+            title: 'React JS Developer',
+            company: 'Express Scripts International',
+            companyWebsite: 'https://express-scripts.com/',
+            location: 'Franklin Lakes, NJ',
+            jobTitle: 'React JS Developer',
+            description: '•	Developed SPA using HTML5/CSS3, SASS, JavaScript/JSX, React JS, Redux, JSON, Node.js, and Ajax Developed the backend by using Node.js, Express and MongoDB <br /> •	Utilized React Server - Side Rendering, to make the website faster by handling UI exceptions at server side <br /> •	Composed Unit Test cases using Jest - Enzyme for the ReactJS components, in a test - driven approach <br /> •	Used Node.js to run Grunt tasks and build properly the project(compile, minify, concat etc.) <br /> •	Developed single application to render dynamically based on the JSON from the Node JS server <br /> •	Used popular Node.js frameworks like Express and Restify to create a Restful Mock API <br /> •	Developed MEAN Stack from scratch including Mongo DB server setup and Express JS server development. <br /> •	Extensive use of Node JS, Express JS, and Mongo DB along with Mongoose.JS to create server side applications <br /> ',
+            userId: 'google-oauth2|101848771434318992569',
+            startDate: '12/01/2018',
+            endDate: '08/01/2019'
+        },
+        {
+            title: 'Fullstack Developer',
+            company: 'Barclay Card USA',
+            companyWebsite: 'https://cards.barclaycardus.com/',
+            location: 'Parsippany, NJ',
+            jobTitle: 'Fullstack Developer',
+            description: '•	Designed Responsive Single Page Application using React JS, JSON, SASS/SCSS <br /> •	Developed an application using React JS, Redux, ES6, and Bootstrap in a Test - Driven Development Environment. <br /> •	Cooperate with UX team and redesigned using React - Bootstrap and React - UI to improve business competencies, also makes the website more attractive. <br /> •	Consume RESTful resources using AJAX call, receiving, analyzing and processing JSON responses to get the required proper - formatted data. <br /> •	Experience in React Js and working with React Redux architecture and using React Js Components, Forms, Event, Keys, Router concepts. <br /> •	Contributed to design and development of the UI for the Credit Card Offers Portal Developed a Rating Results viewing and analysis single page web app portal using React JS. <br /> •	Worked on React JS Virtual Dom and React views, rendering using components which contains additional components called custom HTML tags. <br /> ',
+            userId: 'google-oauth2|101848771434318992569',
+            startDate: '08/01/2019',
+            endDate: '12/01/2019'
+        },
+        {
+            title: 'Fullstack Developer',
+            company: 'Discover Financial Services',
+            companyWebsite: 'https://www.discover.com/',
+            location: 'Parsippany, NJ',
+            jobTitle: 'Fullstack Developer',
+            description: '•	Developed responsive single page applications using NextJS, React JS, ASS, JavaScript/ES6, and Node.js <br /> •	Developed a server using Python Flask with endpoints to provide data to the UI <br /> •	Worked as the application lead to confirm the tech stack for the application <br /> •	Worked with team in developing an interactive menu with elegant animations using D3.js <br /> •	Designed reusable components to consume REST API\'s with Component - based architecture provided by React JS<br /> •	Developed ReactJS UI components using popular JS testing libraries like Jest and Enzyme, in a test - driven approach < br /> •	Used Dynamo DB for faster transactions utilizing the in -memory caching feature provided by dynamo DB <br /> ',
+            userId: 'google-oauth2|101848771434318992569',
+            startDate: '12/15/2019',
+            endDate: '7/01/2020'
+        },
     ],
     blogs: [
         {
